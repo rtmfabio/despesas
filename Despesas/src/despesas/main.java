91e62074f28e3fabe6d05dfbc6c8dd67c5d2a5dd
Fabio Mendonca <Fabio Mendonca@PC-FMENDONCA.divultec.local>
2013-03-14 22:24:37 +0000

    Inicio do Projecto

--- Despesas/src/despesas/main.java
+++ Despesas/src/despesas/main.java
@@ -1,19 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package despesas;
 
 /**
- *
  * @author Fabio Mendonca
  */
+
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Teste Teste Teste \n");
+        
     }
 }
